a58dc2a2754f1608b90b6e11709f2d5951f8c725
xxseva44 <dev.xxseva44@gmail.com>
2022-09-25 19:10:59 -0400

    Add variants, and update the kernels

--- h850/arrow_h850.mk
+++ h850/arrow_h850.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common ROM stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from h815 device
